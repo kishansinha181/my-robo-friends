6f4dd47e9063961e0287e69665600f55279da847
kishansinha181 <kishan.sinha18@gmail.com>
2021-06-27 17:07:59 +0530

    changed styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = ({ name, email, id }) => {
+const Card = ({ name, city, id }) => {
   return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+      <img alt='robots' src={`https://robohash.org/${id + "13"}?size=200x200`} />
       <div>
         <h2>{name}</h2>
-        <p>{email}</p>
+        <p>City : {city}</p>
       </div>
     </div>
   );
